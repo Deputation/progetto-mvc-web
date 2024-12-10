381b3ca069593d6a660e1c2e371a3135aa8cf99a
Deputation <deputation2@gmail.com>
2024-12-10 17:28:45 +0100

    Implemented method for distance calculations between PaymentPoint objects

--- PaymentPointFinder.Core/Models/PaymentPoint.cs
+++ PaymentPointFinder.Core/Models/PaymentPoint.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Numerics;
+using System.Reflection.Metadata;
 using System.Text.Json.Serialization;
 
 namespace PaymentPointFinder.Core.Models;
@@ -45,12 +47,33 @@ public class PaymentPoint
     [JsonPropertyName("NIL")]
     public string NIL { get; set; }
 
+    // Longitude
     [JsonPropertyName("xWGS84")]
     public double xWGS84 { get; set; }
 
+    // Latitude
     [JsonPropertyName("YWGS84")]
     public double YWGS84 { get; set; }
 
     [JsonPropertyName("Location")]
     public string Location { get; set; }
+    
+    private Vector2 ToCartesian()
+    {
+        return new Vector2
+        {
+            X = (float)(6371 * Math.Cos(YWGS84) * Math.Cos(xWGS84)),
+            Y = (float)(6371 * Math.Cos(YWGS84) * Math.Sin(xWGS84))
+        };
+    }
+    
+    public double Distance(PaymentPoint other)
+    {
+        var thisCartesian = ToCartesian();
+        var otherCartesian = other.ToCartesian();
+        var deltaX = thisCartesian.X - otherCartesian.X;
+        var deltaY = thisCartesian.Y - otherCartesian.Y;
+        
+        return Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
+    }
 }
