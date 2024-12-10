21c90300cd7665819ea922e5e7c6a609445ed05a
camb3la <59169021+camb3la@users.noreply.github.com>
2024-12-10 17:39:55 +0100

    Created LocationService and its interface

---
+++ PaymentPointFinder.Core/Services/Interfaces/ILocationService.cs
@@ -0,0 +1,14 @@
+﻿using PaymentPointFinder.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentPointFinder.Core.Services.Interfaces
+{
+    public interface ILocationService
+    {
+        Task<List<PaymentPoint>> GetNearbyPoints(double lat, double lng, double radius);
+    }
+}
---
+++ PaymentPointFinder.Core/Services/LocationService.cs
@@ -0,0 +1,20 @@
+﻿using PaymentPointFinder.Core.Models;
+using PaymentPointFinder.Core.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentPointFinder.Core.Services
+{
+    public class LocationService : ILocationService
+    {
+        public LocationService() { }
+
+        public Task<List<PaymentPoint>> GetNearbyPoints(double lat, double lng, double radius)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
